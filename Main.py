81e45af4e8f6d6451751ca609c1cd73c153302e6
Gian <gianlaager114@gmail.com>
2020-04-05 22:01:32 +0200

    commit for the finished feature

--- Main.py
+++ Main.py
@@ -4,59 +4,98 @@ import numpy as np
 from pytz import timezone
 
 
-def arrays_to_dict(keys, data):
-    if len(keys) != data.shape[0]:
-        raise Exception('keys and data must have the same length')
-
-    dictionary = {}
-    for i in range(len(keys)):
-        dictionary[keys[i]] = data[i]
-
-    return dictionary
-
-
 def timedelta_to_minutes(delta):
     deltaMinutes = delta.astype('timedelta64[m]')
     return deltaMinutes.astype('int')
 
 
-def createDataset(stockName):
-    dfData = yf.download(tickers=stockName, period='60d', interval='2m')
+def mulEachElement(arr):
+    rv = 1
 
-    dates = dfData.index.to_numpy()
+    for v in arr:
+        rv *= v
 
-    for i in range(len(dates)):
-        dates[i] = dates[i].to_numpy()
+    return rv
 
-    rawData = tf.Variable(dfData.to_numpy())
 
-    dictData = arrays_to_dict(dates, rawData)
+class DataGenarator:
+    def __init__(self, stockName):
+        self.sockName = stockName
+        self.dfData = yf.download(
+            tickers=stockName, period='60d', interval='2m')
+        self.dates = self.extractDates()
+        self.dictData = self.convertDataFrameToDict()
+        self.sampleShape, self.lableShape = self.calcShapes()
+        self.sampleDtype = tf.double
+        self.lableDtype = tf.double
 
-    sampleList = []
-    lableList = []
+    def extractDates(self):
+        dates = self.dfData.index.to_numpy()
 
-    for i in range(int(len(list(dictData.keys())) / 2), len(list(dictData.keys()))):
-        print(i - int(len(list(dictData.keys())) / 2), 'of', len(list(dictData.keys())) - int(len(list(dictData.keys())) / 2))
-        cSampleKeys = list(dictData.keys())[i - int(len(list(dictData.keys())) / 2):i]
-        cDate = list(dictData.keys())[i]
+        for i in range(len(dates)):
+            dates[i] = dates[i].to_numpy()
 
-        cSamples = []
-        for date in cSampleKeys:
-            cSamples.append(dictData[date])
+        return dates
 
-        for d in list(dictData.keys())[i+1:]:
-            sample = []
-            sample.append(tf.Variable(timedelta_to_minutes(d - cDate)))
-            for i in range(len(cSamples)):
-                sample.append(cSamples[i])
+    def convertDataFrameToDict(self):
+        rawData = tf.Variable(self.dfData.to_numpy())
 
-            sampleList.append(sample)
-            lableList.append(dictData[d])
-            
+        dictionary = {}
+        for i in range(len(self.dates)):
+            dictionary[self.dates[i]] = rawData[i]
+
+        return dictionary
+
+    def calcShapes(self):
+        sKeys = list(self.dictData.keys())[
+            :int(len(list(self.dictData.keys())) / 2)]
+        s = []
+        for date in sKeys:
+            s.append(self.dictData
+                     [date])
+        s = np.array(s)
+
+        sampleShape = tf.TensorShape(mulEachElement(s.shape) + 1)
+        lableShape = self.dictData[list(self.dictData.keys())[0]].shape
+
+        return sampleShape, lableShape
+
+    def __call__(self):
+        for i in range(int(len(list(self.dictData
+                                    .keys())) / 2), len(list(self.dictData
+                                                             .keys()))):
+            cSampleKeys = list(self.dictData
+                               .keys())[i - int(len(list(self.dictData
+                                                         .keys())) / 2):i]
+            cDate = list(self.dictData
+                         .keys())[i]
+
+            cSamples = []
+            for date in cSampleKeys:
+                cSamples.append(self.dictData
+                                [date])
+
+            cSamples = tf.reshape(tf.Variable(
+                cSamples), (mulEachElement(tf.Variable(cSamples).shape)))
+
+            for d in list(self.dictData
+                          .keys())[i+1:]:
+
+                sample = []
+                sample.append(tf.Variable(timedelta_to_minutes(d - cDate)))
+                for j in range(cSamples.shape[0]):
+                    sample.append(cSamples[j])
+
+                yield tf.Variable(sample, dtype=self.sampleDtype), self.dictData[d].astype(self.lableDtype)
+
 
 def main():
-    stockName = 'AAPL'
-    dataset = createDataset(stockName)
+    STOCKNAME = 'AAPL'
+
+    dataGenarator = DataGenarator(STOCKNAME)
+
+    dataset = tf.data.Dataset.from_generator(
+        dataGenarator, (dataGenarator.sampleDtype, dataGenarator.lableDtype), (dataGenarator.sampleShape, dataGenarator.lableShape))
 
 
 if __name__ == "__main__":
